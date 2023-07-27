8b8c83a172daec5a33c52658381eae6d6c92cf90
Noham334 <israelmateus04@gmail.com>
2023-07-27 17:07:45 -0400

    2. Estructura basica, materialize y composer establecidos

---
+++ src/config/config.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>AMDECO</title>
+   <link rel="stylesheet" href="../src/config/materialize/css/materialize.min.css">
+</head>
+
+<body>
+   <nav class="blue-grey darken-3">This is an NavBar</nav>
+   <script src="../src/config/materialize/js/materialize.min.js"></script>
+</body>
+
+</html>
